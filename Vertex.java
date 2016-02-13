48d644afae008179a65bd27e5b71eeb75eea8223
Donvini <vincenzo.pace94@icloud.com>
2016-02-13 21:08:28 +0100

    Attribut Name hinzugefügt um Stadt zu identifizieren.

--- Vertex.java
+++ Vertex.java
@@ -1,8 +1,21 @@
 package final01;
 
 /**
+ * Diese Klasse representiert eine Stadt in unserem Navigationssystem.
+ *
  * @author Vincenzo Pace | KIT
  * @version 1.0
  */
 public class Vertex {
+
+    // name um Stadt zu identifizieren
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
